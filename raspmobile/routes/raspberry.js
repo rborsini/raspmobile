77be1059a9abf05c7d7281e2faa11f644ff403f1
r.borsini <roberto.borsini@gmail.com>
2015-08-04 15:53:32 +0200

    Struttura base progetto node

---
+++ raspmobile/routes/raspberry.js
@@ -0,0 +1,14 @@
+﻿
+/*
+ * GET relays listing.
+ */
+
+var relays = [];
+
+relays.push({ name: 'Relay_1', status: 'on' });
+relays.push({ name: 'Relay_2', status: 'off' });
+relays.push({ name: 'Relay_3', status: 'on' });
+
+exports.list = function (req, res) {
+    res.send(JSON.stringify(relays));
+};
